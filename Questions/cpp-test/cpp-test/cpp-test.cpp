dc6a7c2fa4d89ca561fac520a5bbd9c6d2969f9b
DESKTOP-GKLETRF\jdsmi <jdsmith104@outlook.com>
2022-05-05 23:31:56 +0100

    Completed C++ question 1. Assumes that there will never be an integer overflow on the object counter. Assuming no objects are being copied. Assuming that new instances of the class are always cleaned up/deleted.

---
+++ Questions/cpp-test/cpp-test/cpp-test.cpp
@@ -0,0 +1,63 @@
+// cpp-test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Expected to be compiled using C++17
+//
+
+#include <iostream>
+
+namespace CppTest
+{
+
+	class TestClass
+	{
+
+	public:
+		TestClass()
+		{
+			sNumObjects++;
+		}
+		
+		~TestClass()
+		{
+			sNumObjects--;
+		}
+
+		static int nobjs()
+		{
+			return sNumObjects;
+		}
+
+
+	private:
+		// Declared as int so the value may overflow to a negative number
+		inline static int sNumObjects = 0;
+
+	};
+
+	static void PrintObjectCount()
+	{
+		std::cout << "Object count: " << CppTest::TestClass::nobjs() << std::endl;
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+	const std::unique_ptr<CppTest::TestClass> testObject1(new CppTest::TestClass());
+	CppTest::PrintObjectCount();
+
+	for (auto i = 0; i < 5; ++i)
+	{
+		const std::unique_ptr<CppTest::TestClass> testObject(new CppTest::TestClass());
+		CppTest::PrintObjectCount();
+	}
+
+	const std::unique_ptr<CppTest::TestClass> testObject2(new CppTest::TestClass());
+	CppTest::PrintObjectCount();
+	{
+		const std::unique_ptr<CppTest::TestClass> testObject3(new CppTest::TestClass());
+		CppTest::PrintObjectCount();
+	}
+	
+	CppTest::PrintObjectCount();
+
+}
