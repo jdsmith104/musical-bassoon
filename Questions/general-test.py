f3ca1ab9ab3d95d699c54a98e3156d4c0cd32e97
DESKTOP-GKLETRF\jdsmi <jdsmith104@outlook.com>
2022-05-05 22:30:33 +0100

    Asnwered question 1. Had to use floats because the precision of python is lower than excel

--- Questions/general-test.py
+++ Questions/general-test.py
@@ -1,3 +1,4 @@
+import math
 import unittest
 
 
@@ -33,6 +34,24 @@ class FibonacciNumberGenerator:
             self.calculated_result[x] = self.calculated_result[x -
                                                                1] + self.calculated_result[x-2]
             return self.calculated_result[x]
+
+    # Answer to Q1. Every third fibonacci number from f(1) is even
+    def solve_question(self, n: int = 100) -> int:
+
+        # 100th even fibonacci number index
+        step = 3
+        end_index = n * 3
+
+        # Pre calculate all answers
+        self.get_number(end_index)
+
+        fib_sum = 0
+
+        # _fib() should resolve in O(1)
+        for i in range(0, end_index, step):
+            fib_sum += self._fib(i)
+
+        return fib_sum
 
 
 class FibonacciNumberGeneratorTest(unittest.TestCase):
@@ -71,6 +90,37 @@ class FibonacciNumberGeneratorTest(unittest.TestCase):
         n = 1
         expected_output = 1
         self.assertEqual(expected_output, fng.get_number(n))
+
+    def test_case_7(self):
+        fng = FibonacciNumberGenerator()
+        n = 5
+        expected_output = 188
+        output = fng.solve_question(n)
+        self.assertEqual(expected_output, output)
+
+    def test_case_8(self):
+        fng = FibonacciNumberGenerator()
+        n = 10
+        expected_output = 257114
+        output = fng.solve_question(n)
+        self.assertEqual(expected_output, output)
+
+    def test_case_9(self):
+        fng = FibonacciNumberGenerator()
+        n = 20
+        expected_output = 478361013020
+        output = fng.solve_question(n)
+        self.assertEqual(expected_output, output)
+
+    def test_question_1(self):
+        fng = FibonacciNumberGenerator()
+        n = 100
+        # Calculated manually using excel
+        expected_output: int = 68673540288581500000000000000000000000000000000000000000000000
+        expected_output_scaled: float = expected_output / math.pow(10, 61)
+        output = fng.solve_question(n)
+        output_scaled: float = output / math.pow(10, 61)
+        self.assertAlmostEqual(expected_output_scaled, output_scaled, 7)
 
 
 if __name__ == "__main__":
